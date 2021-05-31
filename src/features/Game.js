04d23e1df41ee58c29aaffa6be54be670b30eab3
eXebyss <exeleroxz@gmail.com>
2021-05-31 23:51:59 +0300

    Winner squares highlighting enabled

--- src/features/Game.js
+++ src/features/Game.js
@@ -3,94 +3,90 @@ import calculateWinner from '../features/CalculateWinner';
 import Board from '../features/Board';
 
 class Game extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        history: [
-          {
-            squares: Array(9).fill(null)
-          }
-        ],
-        stepNumber: 0,
-        xIsNext: true,
-      };
+  constructor(props) {
+    super(props);
+    this.state = {
+      history: [
+        {
+          squares: Array(9).fill(null)
+        }
+      ],
+      stepNumber: 0,
+      xIsNext: true,
+    };
+  }
+  
+  handleClick(i) {
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
     }
+    squares[i] = this.state.xIsNext ? "X" : "O";
+    this.setState({
+      history: history.concat([
+        {
+          squares: squares
+        }
+      ]),
+      stepNumber: history.length,
+      xIsNext: !this.state.xIsNext
+    });
+  }
   
-    handleClick(i) {
-      const history = this.state.history.slice(0, this.state.stepNumber + 1);
-      const current = history[history.length - 1];
-      const squares = current.squares.slice();
-      if (calculateWinner(squares) || squares[i]) {
-        return;
-      }
-      squares[i] = this.state.xIsNext ? "X" : "O";
-      this.setState({
-        history: history.concat([
-          {
-            squares: squares
-          }
-        ]),
-        stepNumber: history.length,
-        xIsNext: !this.state.xIsNext
-      });
-    }
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0
+    });
+  }
   
-    jumpTo(step) {
-      this.setState({
-        stepNumber: step,
-        xIsNext: (step % 2) === 0
-      });
-    }
-  
-    render() {
-      const history = this.state.history;
-      const current = history[this.state.stepNumber];
-      const winner = calculateWinner(current.squares);
-  
-      const moves = history.map((step, move) => {
-        const desc = move ?
-          'Go to move #' + move :
-          'Go to game start';
-        return (
-          <li key={move}>
-            <button onClick={() => this.jumpTo(move)}>{desc}</button>
-          </li>
-        );
-      });
-  
-      let status;
-      let winnerStyleColor;
-      let winnerStyleBackgroundColor;
-      if (winner) {
-        status = "Winner: " + winner;
-        if (winner === "X") {
-          winnerStyleColor = {color: "yellowgreen"};
-          winnerStyleBackgroundColor = {backgroundColor: "yellowgreen"};          
-        } else {
-          winnerStyleBackgroundColor = {backgroundColor: "orange"};
-          winnerStyleColor = {color: "orange"};
-        }        
-      } else {
-        status = "Next player: " + (this.state.xIsNext ? "X" : "O");
-      }
-
+  render() {
+    const history = this.state.history;
+    const current = history[this.state.stepNumber];
+    const winner = calculateWinner(current.squares);
+    
+    const moves = history.map((step, move) => {
+      const desc = move ?
+        'Go to move #' + move:
+        'Go to game start';
       return (
-        <div className="game">
-          <div className="game-board">
-            <Board
-              squares={current.squares}
-              onClick={i => this.handleClick(i)}
-              winnerStyleBackgroundColor={winnerStyleBackgroundColor}
-              winner={winner}
-            />
-          </div>
-          <div className="game-info">
-            <div style={winnerStyleColor}>{status}</div>
-            <ol>{moves}</ol>
-          </div>
-        </div>
+        <li key={move}>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
       );
+    });
+  
+    let status;
+    let winnerStyleColor;
+    if (winner) {
+      status = "Winner: " + winner;
+      if (winner === "X") {
+        winnerStyleColor = {color: "yellowgreen"};          
+      } else {
+        winnerStyleColor = {color: "orange"};
+      }        
+    } else {
+      status = "Next player: " + (this.state.xIsNext ? "X" : "O");
     }
+
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board
+            squares={current.squares}
+            onClick={i => this.handleClick(i)}
+            winner={winner}
+          />
+        </div>
+        <div className="game-info">
+          <div style={winnerStyleColor}>{status}</div>
+          <ol>{moves}</ol>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Game;
--- src/features/Square.js
+++ src/features/Square.js
@@ -1,10 +1,19 @@
 function Square(props) {
+  let color;
+  if (props.winner) {
+    if (props.winner === 'X' && props.value === 'X') {
+      color = {color: 'green', backgroundColor: 'yellowgreen'}
+    } else if (props.winner === 'O' && props.value === 'O') {
+      color = {color: 'orange', backgroundColor: 'yellow'}
+    }
+  }
 
   return (
     <button 
       className="square" 
       onClick={props.onClick}
-      style={props.winnerStyleBackgroundColor}>
+      style={color}
+      >
       {props.value}
     </button>
   );
